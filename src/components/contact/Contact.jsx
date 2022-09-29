147da6e8515c397f6173b8a6d5c6d79f6311c32f
TonnyNguyen23 <nguyenxuantung473@gmail.com>
2022-09-29 17:13:08 +0700

    [1.0.0] Build and Responsive Contact page

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,75 @@
 import React from "react";
 import "./contact.css";
 
+import { MdOutlineEmail } from "react-icons/md";
+import { RiMessengerFill } from "react-icons/ri";
+import { BsSkype } from "react-icons/bs";
+
 const Contact = () => {
-  return <section id="contact">Contact</section>;
+  return (
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail className="contact__option-icon" />
+            <h4>Email</h4>
+            <h5>nguyenxuantung473@gmail.com</h5>
+            <a
+              href="mailto:nguyenxuantung473@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send a message
+            </a>
+          </article>
+
+          <article className="contact__option">
+            <RiMessengerFill className="contact__option-icon" />
+            <h4>Messenger</h4>
+            <h5>Xuan Tung</h5>
+            <a href="https://m.me/xuantung23" target="_blank" rel="noreferrer">
+              Send a message
+            </a>
+          </article>
+
+          <article className="contact__option">
+            <BsSkype className="contact__option-icon" />
+            <h4>Skype</h4>
+            <h5>+84 0364986914</h5>
+            <a
+              href="skype:live:.cid.3c7903626b4fd8a5?chats"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send a message
+            </a>
+          </article>
+        </div>
+        {/* END OF CONTACT OPTIONS */}
+        <form action="">
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
